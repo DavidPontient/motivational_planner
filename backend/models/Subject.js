9671c469622781939709987b7a939097369d6395
DavidPontient <d.kayumba1@alustudent.com>
2025-11-27 15:11:30 -0600

    Update Subject.js

--- backend/models/Subject.js
+++ backend/models/Subject.js
@@ -1,1 +1,7 @@
-
+const mongoose = require('mongoose');
+const schema = new mongoose.Schema({
+  title: {type:String, required:true},
+  hours: {type:Number, default:0},
+  created: {type:Date, default: Date.now}
+});
+module.exports = mongoose.model('Subject', schema);
