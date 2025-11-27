14a45d59feb02c6dec87637a71b6532d7769c680
DavidPontient <d.kayumba1@alustudent.com>
2025-11-27 17:37:49 -0600

    Delete backend directory

--- backend/models/Note.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  title: String,
-  body: String,
-  created: {type:Date, default: Date.now}
-});
-module.exports = mongoose.model('Note', schema);
--- backend/models/Session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  desc: {type:String, required:true},
-  date: {type:String, required:true}, // store as ISO date string (yyyy-mm-dd)
-  start: {type:String, required:true}, // HH:MM
-  end: {type:String, required:true},
-  created: {type:Date, default: Date.now}
-});
-module.exports = mongoose.model('Session', schema);
