11e54b5c55eab71f3173efa5c7717ed2ebd1360f
DavidPontient <d.kayumba1@alustudent.com>
2025-11-27 18:46:34 -0600

    Update script.js

--- Asset/script.js
+++ Asset/script.js
@@ -1,273 +1,160 @@
-// ----------------------- Script Overview ----------------------- //
-
-// Use Cases
-
-
-
-// ----------------------- Todo list Variables ----------------------- //
-
+// ----------------------- Todo List Variables ----------------------- //
 var todoInput = document.querySelector("#todo-text");
-
 var todoForm = document.querySelector("#todo-form");
-
 var todoList = document.querySelector("#todo-list");
-
 var todoCountSpan = document.querySelector("#todo-count");
-// create a todos array to hold todos text
-var todos = [];
-// ----------------------- Todo list Functions ----------------------- //
-init();
-$("#points").text(localStorage.clickcount);
-function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
 
+// Create an array to hold todos
+var todos = [];
+
+// ----------------------- Initialize ----------------------- //
+init();
+$("#points").text(localStorage.clickcount || 0);
+
+// ----------------------- Todo List Functions ----------------------- //
+function init() {
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+  if (storedTodos) todos = storedTodos;
+  renderTodos();
+}
+
+function storeTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function renderTodos() {
+  todoList.innerHTML = "";
   todoCountSpan.textContent = todos.length;
 
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-
+  todos.forEach((todo, i) => {
     var li = document.createElement("li");
     li.textContent = todo;
     li.setAttribute("data-index", i);
 
-
     var deleteButton = document.createElement("button");
-    deleteButton.classList.add("fa", "fa-close", "btn", "btn-danger", "btn-lg");
-
+    deleteButton.className = "fa fa-close btn btn-danger btn-lg";
 
     var countdownButton = document.createElement("button");
-    countdownButton.classList.add("fa", "fa-hourglass-1", "btn", "btn-primary", "btn-lg");
-
+    countdownButton.className = "fa fa-hourglass-1 btn btn-primary btn-lg";
 
     var completeButton = document.createElement("button");
-    completeButton.classList.add("fa", "fa-check-square-o", "btn", "btn-success", "btn-lg");
+    completeButton.className = "fa fa-check-square-o btn btn-success btn-lg";
 
     li.appendChild(deleteButton);
     li.appendChild(countdownButton);
     li.appendChild(completeButton);
 
     todoList.appendChild(li);
-  }
+  });
 }
 
-function init() {
-  // Get stored todos from localStorage
-  // Parsing the JSON string to an object
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
-  }
-
-  // Render todos to the DOM
-  renderTodos();
-}
-
-function storeTodos() {
-  // Stringify and set "todos" key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// When form is submitted...
+// ----------------------- Event Listeners ----------------------- //
 todoForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  // clear the whitespace from text entered by the user
   var todoText = todoInput.value.trim();
+  if (!todoText) return;
 
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
-  }
-
-  // Add new todoText to todos array, clear the input
   todos.push(todoText);
-  // clear the text from todos textbox
   todoInput.value = "";
 
-  // Store updated todos in localStorage, re-render the list
   storeTodos();
-  // display the todo to the user
   renderTodos();
 });
 
-// When a element inside of the todoList is clicked...
 todoList.addEventListener("click", function (event) {
   var element = event.target;
+  var index = element.parentElement.getAttribute("data-index");
 
-  // If that element is a button...
-  if (element.classList.contains("fa-close") == true) {
-    // Get its data-index value and remove the todo element from the list
-    var index = element.parentElement.getAttribute("data-index");
+  if (element.classList.contains("fa-close")) {
     todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
     storeTodos();
-    // display the todo to the user
     renderTodos();
-  } else if (element.classList.contains("fa-hourglass-1") == true) {
-    startTimer();
-  } else {
-    var index = element.parentElement.getAttribute("data-index");
+  } else if (element.classList.contains("fa-hourglass-1")) {
+    startTimer(5, "#time"); // Start 5-minute timer
+  } else if (element.classList.contains("fa-check-square-o")) {
     todos.splice(index, 1);
-    // Update points counter
     clickCounter();
-    // Store updated todos in localStorage, re-render the list
     storeTodos();
-    // display the todo to the user
     renderTodos();
   }
 });
 
-
+// ----------------------- Click Counter ----------------------- //
 function clickCounter() {
-  if (typeof (Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount) + 1;
-      $("#points").text(localStorage.clickcount);
-    } else {
-      localStorage.clickcount = 1;
-    }
+  if (typeof Storage !== "undefined") {
+    localStorage.clickcount = (Number(localStorage.clickcount) || 0) + 1;
+    $("#points").text(localStorage.clickcount);
   }
 }
-// ----------------------- Countdown Timer Start ----------------------- //
 
+// ----------------------- Countdown Timer ----------------------- //
+function startTimer(minutes = 5, displaySelector = "#time") {
+  var duration = moment.duration({ minutes, seconds: 0 });
+  var display = $(displaySelector);
 
-var startTimer = function () {
-  var duration = moment.duration({
-    "minutes": 0,
-    "seconds": 10
-  });
-  //    moments.js 
-  var timestamp = new Date(0, 0, 0, 2, 10, 30);
-  var interval = 1;
   var timer = setInterval(function () {
-    timestamp = new Date(timestamp.getTime() + interval * 1000);
-    duration = moment.duration(duration.asSeconds() - interval, "seconds");
     var min = duration.minutes();
     var sec = duration.seconds();
-    sec -= 1;
-    if (min < 0) return clearInterval(timer);
-    if (min < 10 && min.length != 2) min = "0" + min;
-    if (sec < 0 && min != 0) {
-      min -= 1;
-      sec = 59;
-    } else if (sec < 10 && sec.length != 2) sec = "0" + sec;
-    $("#time").text(min + ":" + sec);
-    if (min == 0 && sec == 0)
+
+    // Format with leading zeros
+    var formattedMin = min < 10 ? "0" + min : min;
+    var formattedSec = sec < 10 ? "0" + sec : sec;
+    display.text(formattedMin + ":" + formattedSec);
+
+    duration = moment.duration(duration.asSeconds() - 1, "seconds");
+
+    if (duration.asSeconds() <= 0) {
       clearInterval(timer);
+    }
   }, 1000);
 }
 
-$("#timerButton").on("click", function timmerClick() {
-  var fiveMinutes = 60 * 5,
-    display = document.querySelector('#time');
-  startTimer(fiveMinutes, display);
+$("#timerButton").on("click", function () {
+  startTimer(5, "#time"); // Default 5-minute timer
 });
 
-
-
-var startTimer = function(){
-  var duration = moment.duration({
-    "minutes": 5,
-    "seconds": 00
-
-  });
-//    moments.js 
-  var timestamp = new Date(0, 0, 0, 2, 10, 30);
-  var interval = 1;
-  var timer = setInterval(function() {
-    timestamp = new Date(timestamp.getTime() + interval * 1000);
-
-    duration = moment.duration(duration.asSeconds() - interval, "seconds");
-    var min = duration.minutes();
-    var sec = duration.seconds();
-
-
-    sec -= 1;
-    if (min < 0) return clearInterval(timer);
-    if (min < 10 && min.length != 2) min = "0" + min;
-    if (sec < 0 && min != 0) {
-      min -= 1;
-      sec = 59;
-    } else if (sec < 10 && sec.length != 2) sec = "0" + sec;
-
-
-
-    $("#time").text(min + ":" + sec);
-    if (min == 0 && sec == 0)
-      clearInterval(timer);
-
-
-  }, 1000);
-  }
-
-// ----------------------- Quote API Variables ----------------------- //
-
-
-// quotegarden API
+// ----------------------- Quote API ----------------------- //
 var queryURL = "https://quote-garden.herokuapp.com/api/v2/quotes/random?";
+var quoteText = $("#quote");
 
-// Quotes on Design API
-
-// ----------------------- Filtered Quotes API Variables (currently in test phase)----------------------- //
-// var genreName = ["be-great", "effort", "goals", "procrastination", "action"];
-// var randomQuoteNumber = Math.floor(Math.random() * genreName.length);
-
-// random genre API call
-// var randomQuoteURL = "https://quote-garden.herokuapp.com/api/v2/genre/:" + genreName[randomQuoteNumber] + "?page=1&limit=10"
-// ----------------------- Quote API Functions ----------------------- //
-
-// Quote Garden API call
-var quoteText = $("#quote")
 $("#motivationButton").click(function () {
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    quoteText.empty();
-    quoteText.append(response.quote.quoteText + "\n" + "- " + response.quote.quoteAuthor);
+    quoteText.text(`${response.quote.quoteText} - ${response.quote.quoteAuthor}`);
   });
 });
 
-// ----------------------- Images API Variables ----------------------- //
+// ----------------------- Images API ----------------------- //
 var randomImageNumber = Math.floor(Math.random() * 20);
-var imageToDisplay = $("#motivationImage")
+var imageToDisplay = $("#motivationImage");
 var pixelKey = "17203059-4d033efc49ecc457a7083a895";
-
-var imgUrl = "https://pixabay.com/api/?key=" + pixelKey + "&category=places&image_type=photo&orientation=horizontal";
-
-// Accepted category values: backgrounds, fashion, nature, science, education, feelings, health, people, religion, places, animals, industry, computer, food, sports, transportation, travel, buildings, business, music
-
-// ----------------------- Images API Functions ----------------------- //
+var imgUrl = `https://pixabay.com/api/?key=${pixelKey}&category=places&image_type=photo&orientation=horizontal`;
 
 function getImages() {
-
   $.ajax({
     url: imgUrl,
     method: "GET",
   }).then(function (imageData) {
-    imageToDisplay.attr("src", imageData.hits[randomImageNumber].webformatURL);
+    if (imageData.hits && imageData.hits[randomImageNumber]) {
+      imageToDisplay.attr("src", imageData.hits[randomImageNumber].webformatURL);
+    }
   });
 }
 
 getImages();
 
-// ----------------------- Second Images API Functions (currently in test phase) ----------------------- //
-
-var secondImageUrl = "https://api.unsplash.com/photos/random?client_id=yeduKBA2kZ723vugi0TIdMCjX0EG523F5QhrhuwlLdg"
+// ----------------------- Second Images API (Test) ----------------------- //
+var secondImageUrl = "https://api.unsplash.com/photos/random?client_id=yeduKBA2kZ723vugi0TIdMCjX0EG523F5QhrhuwlLdg";
 
 function getNewImages() {
-
   $.ajax({
     url: secondImageUrl,
     method: "GET",
-    scope: "public"
+    scope: "public",
   }).then(function (upData) {
-    console.log("working!")
-    console.log(upData[2]);
+    console.log("working!");
+    console.log(upData);
   });
 }
-
