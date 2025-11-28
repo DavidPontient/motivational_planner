8bd858e1d416cd01094f3463faeffca3c1729d68
DavidPontient <d.kayumba1@alustudent.com>
2025-11-27 18:33:41 -0600

    Update landing.js

--- landing.js
+++ landing.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
-radius = radius * 0.90;
+radius = radius * 0.9;
 setInterval(drawClock, 1000);
 
 function drawClock() {
@@ -15,7 +15,7 @@ function drawClock() {
 function drawFace(ctx, radius) {
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = '#111';
     ctx.fill();
 
     var grad = ctx.createRadialGradient(0, 0, radius*0.95, 0, 0, radius*1.05);
@@ -28,7 +28,7 @@ function drawFace(ctx, radius) {
 
     ctx.beginPath();
     ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-    ctx.fillStyle = '#333';
+    ctx.fillStyle = '#ff4d4d';
     ctx.fill();
 }
 
@@ -41,6 +41,7 @@ function drawNumbers(ctx, radius) {
         ctx.rotate(ang);
         ctx.translate(0, -radius*0.85);
         ctx.rotate(-ang);
+        ctx.fillStyle = "#fff";
         ctx.fillText(num.toString(), 0, 0);
         ctx.rotate(ang);
         ctx.translate(0, radius*0.85);
@@ -50,12 +51,11 @@ function drawNumbers(ctx, radius) {
 
 function drawTime(ctx, radius) {
     var now = new Date();
-    var hour = now.getHours();
+    var hour = now.getHours()%12;
     var minute = now.getMinutes();
     var second = now.getSeconds();
 
-    hour = hour % 12;
-    hour = (hour * Math.PI / 6) + (minute * Math.PI/(6*60)) + (second*Math.PI/(360*60));
+    hour = (hour*Math.PI/6) + (minute*Math.PI/(6*60)) + (second*Math.PI/(360*60));
     drawHand(ctx, hour, radius*0.5, radius*0.07);
 
     minute = (minute*Math.PI/30) + (second*Math.PI/(30*60));
@@ -69,9 +69,9 @@ function drawHand(ctx, pos, length, width) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.lineCap = "round";
-    ctx.moveTo(0, 0);
+    ctx.moveTo(0,0);
     ctx.rotate(pos);
-    ctx.lineTo(0, -length);
+    ctx.lineTo(0,-length);
     ctx.stroke();
     ctx.rotate(-pos);
 }
@@ -81,6 +81,22 @@ $(function() {
     $("#card-1").flip({
         axis: "Y",
         trigger: "click",
-        speed: 200
+        speed: 400
     });
 });
+
+// ---------------- QUOTES -----------------
+const quotes = [
+    {text:"You don't have to see the whole staircase, just take the first step.", author:"Martin Luther King Jr."},
+    {text:"Do something today that your future self will thank you for.", author:"Unknown"},
+    {text:"Success is not final, failure is not fatal: it is the courage to continue that counts.", author:"Winston Churchill"},
+    {text:"Little by little, a little becomes a lot.", author:"Unknown"},
+    {text:"Consistency is what transforms average into excellence.", author:"Unknown"}
+];
+
+document.getElementById("getQuote").addEventListener("click", function(){
+    const random = quotes[Math.floor(Math.random()*quotes.length)];
+    const block = document.getElementById("quoteBlock");
+    block.innerHTML = `<p>${random.text}</p><cite>${random.author}</cite>`;
+});
+
